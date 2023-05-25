2ac4864efe94796f132b2674b6ce5264a5bfe370
Vivian Sanchez Desktop <visasal88@gmail.com>
2023-05-25 13:33:47 -0300

    aulas 7 a 10 finalizadas

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,3 +1,18 @@
+export class GithubUser {
+  static search(username) {
+    const endpoint = `https://api.github.com/users/${username}`
+
+    return fetch(endpoint)
+      .then(data => data.json())
+      .then(({ login, name, public_repos, followers }) => ({
+        login,
+        name,
+        public_repos,
+        followers,
+      }))
+  }
+}
+
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
@@ -5,20 +20,18 @@ export class Favorites {
   }
 
   load() {
-   this.entries = [
-      {
-      login: 'viviansanchez',
-      name: 'Vivian Sanchez',
-      public_repos: '49',
-      followers: '10'
-     },
-     {
-      login: 'maykbrito', 
-      name: 'Mayk Brito',
-      public_repos: '76', 
-      followers: '12000'
-     }
-    ]
+   this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+  }
+
+  async add(username) {
+    const user = await GithubUser.search(username)
+  }
+
+  delete(user) {
+    const filteredEntries = this.entries.filter((entry) => entry.login !== user.login)
+
+    this.entries = filteredEntries
+    this.update()
   }
 }
 
@@ -29,6 +42,16 @@ export class FavoritesView extends Favorites {
     this.tbody = this.root.querySelector('table tbody')
 
     this.update()
+    this.onadd()
+  }
+
+  onadd() {
+    const addButton = this.root.querySelector('.search button')
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('.search input')
+
+      this.add(value)
+    }
   }
 
   update() {
@@ -43,6 +66,14 @@ export class FavoritesView extends Favorites {
       row.querySelector('.user span').textContent = user.login
       row.querySelector('.repositories').textContent = user.public_repos
       row.querySelector('.followers').textContent = user.followers
+
+      row.querySelector('.remove').onclick = () => {
+        const isOk = confirm('Tem certeza que deseja deletar essa linha?') 
+        if(isOk) {
+          this.delete(user)
+        }
+        
+      }
 
       this.tbody.append(row)
     })
